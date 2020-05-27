8ed45ab0248b8f4fa212f562d5a78c873294ba06
Cuong <le.manhcuong.hd1996.1@gmail.com>
2020-05-27 21:29:13 +0700

    Adding method was_published_recently
      Just a custom method

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -17,3 +19,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
